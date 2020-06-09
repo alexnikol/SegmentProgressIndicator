0bae4cf38da213150821d6a40831222cbf7a89b1
Alexander Nikolaychuk <alexksnikol@gmail.com>
2020-06-09 21:14:18 +0300

    first commit

---
+++ ANSegmentIndicator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ANSegmentIndicator
+//
+//  Created by Alexander Nikolaychuk on 09.06.2020.
+//  Copyright © 2020 AlexAlmostEngineer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
